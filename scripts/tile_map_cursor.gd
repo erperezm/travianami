f24bf7452677d8a3f128ecdfc3950bfd7e2c5bd4
Paul Pérez <dnv7.soft@gmail.com>
2024-10-30 09:52:25 -0300

    world map added

---
+++ scripts/tile_map_cursor.gd
@@ -0,0 +1,35 @@
+extends TileMapLayer
+
+@onready var tile_map_world: TileMapLayer = $"."
+@onready var mouse_grid_selected: Sprite2D = $"../MouseGridSelected"
+
+var GridSize = 4
+var Dic = {}
+
+# Called when the node enters the scene tree for the first time. 
+# Part of the code generation investigation - DO NOT DELETE YET
+#func _ready() -> void:
+	## Create the cell based on the sprite index
+	#for x in GridSize:
+		#for y in GridSize:
+			#set_cell(Vector2(x, y), [index-here], Vector2i(0, 0))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	var mouseCoords = local_to_map(get_local_mouse_position())
+	var gridCoords = (mouseCoords * 16) + Vector2i(8,8)
+	mouse_grid_selected.global_position = gridCoords
+	#print(mouseCoords)
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			var mouseCoords = local_to_map(get_local_mouse_position())
+			var zIndex = tile_map_world.get_cell_tile_data(mouseCoords).z_index
+			print("MouseCoords: ", mouseCoords)
+			print("Terrain type: ", zIndex)
+	
