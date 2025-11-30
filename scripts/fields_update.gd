b725147e3417adc07937ef165f90512084ed0e97
Ernesto Perez (Sn0w) <reactsn0wjs@gmail.com>
2025-11-29 21:08:01 -0500

    Fix: loading player_village from loaded to keep game state

--- scripts/fields_update.gd
+++ scripts/fields_update.gd
@@ -2,35 +2,21 @@ extends Node
 
 #Parent node (all buttons container)
 @onready var fields_parent_node = $"."
-var player_village = null
 
 func _ready() -> void:
-	continuous_timer() 
-
-# Infinite timer. Deamon for update village data in {WAIT_TIME_INTERVAL} time.
-const WAIT_TIME_INTERVAL = 2
-func continuous_timer() -> void:
-	var timer = Timer.new()
-	timer.wait_time = WAIT_TIME_INTERVAL 
-	timer.autostart = true
-	timer.one_shot = false 
-	add_child(timer)
-	timer.timeout.connect(_on_continuous_timer_timeout)
-func _on_continuous_timer_timeout():
-	if(HttpRequest_G.loaded): 
-		player_village = HttpRequest_G.villages[ENV.SETTLEMENT_INDEX]
-		set_resource_level()
-
+	ENV.connect("player_village_loaded", Callable(self, "_on_player_village_loaded"))
+	
+	
 # Iterate over all --field button nodes-- setting [level] and [tooltip] text
 func set_resource_level():
-	if player_village:
+	if ENV.player_village:
 		var field_button_index = 0
 		var field = null
 		
 		# Iterate over all 18 field buttons
 		for field_button in fields_parent_node.get_children():
 			if field_button is Button:
-				field = player_village.field.resources[field_button_index]
+				field = ENV.player_village.field.resources[field_button_index]
 				
 				field_button.text = str(field.level)
 				field_button.tooltip_text = "APH:" + str(field.amountPerHour).split(".", true, 0)[0]
@@ -39,11 +25,11 @@ func set_resource_level():
 
 # Update field level  [requested by buttons]
 func update_resource_field(field_button: BaseButton) -> void:
-	if player_village:
+	if ENV.player_village:
 		var field_button_index = field_button.get_meta("index")
 
-		HttpRequest_G.p_request(player_village.Id, player_village.field.resources[field_button_index].Id)
-
+		HttpRequest_G.p_request(ENV.player_village.Id, ENV.player_village.field.resources[field_button_index].Id)
+		
 		set_resource_level()
 
 # This signal comes from loader script when <<payer_village>> variable is setted
