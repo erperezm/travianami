6c37864e3148b9b200bdf884a597cdf971152f9c
Ernesto Perez (Sn0w) <reactsn0wjs@gmail.com>
2025-12-03 23:21:24 -0500

    refactor(http): Clean up whitespace and improve comments for clarity

--- scripts/http.gd
+++ scripts/http.gd
@@ -14,7 +14,7 @@ var http_get: HTTPRequest
 var http_post: HTTPRequest
 var http_get_all_villages: HTTPRequest
 
-
+# Load all villages now only works for world map
 func set_http_all_villages_nodes(hget: HTTPRequest) -> void:
 	http_get_all_villages = hget
 	if http_get_all_villages:
@@ -22,7 +22,6 @@ func set_http_all_villages_nodes(hget: HTTPRequest) -> void:
 		_refresh_all_villages()	
 func _refresh_all_villages() -> void:
 	http_get_all_villages.request("%s/village"%BASE_URL)
-	
 func _on_get_all_village_completed(_result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
 	if response_code == 200:
 		var json = JSON.new()
@@ -31,7 +30,7 @@ func _on_get_all_village_completed(_result: int, response_code: int, _headers: P
 			print("all_villages")
 	_refresh_all_villages()
 
-
+# Load player village and update player villages resources now works only in village view
 func set_http_nodes(hget_node: HTTPRequest, hpost_node: HTTPRequest) -> void:
 	http_get = hget_node
 	http_post = hpost_node
@@ -40,12 +39,9 @@ func set_http_nodes(hget_node: HTTPRequest, hpost_node: HTTPRequest) -> void:
 	if http_post:
 		http_post.request_completed.connect(_on_post_completed)
 	_refresh_villages()	
-
-
 func _refresh_villages() -> void:
 	get_queue.append("%s/village/%s"%[BASE_URL,PLAYER_VILLAGE_ID])
 	_process_get_queue()
-
 func upgrade_resource(village_id: String, resource_id: String) -> void:
 	var url = "https://ernesto.santos.edu.do/village/%s/upgrade/resource/%s" %[village_id, resource_id]
 	post_queue.append({
@@ -55,13 +51,11 @@ func upgrade_resource(village_id: String, resource_id: String) -> void:
 	if post_queue_empty:
 		post_queue_empty = false
 		_process_post_queue()
-
 func _process_get_queue() -> void:
 	if get_queue.is_empty():
 		return
 	var url = get_queue.pop_front()
 	http_get.request(url)
- 
 func _process_post_queue() -> void:
 	if post_queue.is_empty() or http_post.get_http_client_status() in [HTTPClient.STATUS_CONNECTING, HTTPClient.STATUS_RESOLVING, HTTPClient.STATUS_REQUESTING]:
 		print("fin")
@@ -78,7 +72,6 @@ func _on_get_completed(_result: int, response_code: int, _headers: PackedStringA
 			print("player_village")
 	#get_tree().create_timer(0.5).timeout
 	_refresh_villages()
-
 func _on_post_completed(_result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
 	if response_code == 200:
 		var json = JSON.new()
